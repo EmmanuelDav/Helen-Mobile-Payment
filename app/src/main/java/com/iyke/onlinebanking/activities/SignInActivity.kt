7dd9005d331056ef98434ed3d62785be20de341e
emmanueldav <kuradavis987@gmail.com>
2022-07-06 15:53:37 +0100

    added welcome screen

---
+++ app/src/main/java/com/iyke/onlinebanking/activities/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.iyke.onlinebanking.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.iyke.onlinebanking.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
