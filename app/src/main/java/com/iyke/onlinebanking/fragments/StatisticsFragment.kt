7453cbc9e13578244e5b43ebd39af63006ee311f
emmanueldav <kuradavis987@gmail.com>
2022-07-09 18:36:10 +0100

    set statisctis view

--- app/src/main/java/com/iyke/onlinebanking/fragments/StatisticsFragment.kt
+++ app/src/main/java/com/iyke/onlinebanking/fragments/StatisticsFragment.kt
@@ -5,17 +5,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
-import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.charts.PieChart
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.data.*
-import com.github.mikephil.charting.formatter.PercentFormatter
 import com.github.mikephil.charting.utils.ColorTemplate
 import com.iyke.onlinebanking.R
-import java.text.DecimalFormat
 
 
 class StatisticsFragment : Fragment() {
@@ -38,10 +35,15 @@ class StatisticsFragment : Fragment() {
         chart.animateXY(2000, 2000)
         chart.invalidate()
 
-        // pie
-        pieChart = v.findViewById(R.id.activity_main_piechart);
-        setupPieChart();
-        loadPieChartData();
+        val spinnerPeriods: Spinner = v.findViewById(R.id.spinnerPeriods)
+        val adapter = ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.periods,
+            android.R.layout.simple_spinner_item
+        )
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
+        spinnerPeriods.adapter = adapter
 
         return v
     }
@@ -66,45 +68,4 @@ class StatisticsFragment : Fragment() {
         return barEntriesList
     }
 
-    private fun loadPieChartData() {
-        val entries: ArrayList<PieEntry> = ArrayList()
-        entries.add(PieEntry(0.2f, "Food & Dining"))
-        entries.add(PieEntry(0.15f, "Medical"))
-        entries.add(PieEntry(0.10f, "Entertainment"))
-        entries.add(PieEntry(0.25f, "Electricity and Gas"))
-        entries.add(PieEntry(0.3f, "Housing"))
-        val colors: ArrayList<Int> = ArrayList()
-        for (color in ColorTemplate.MATERIAL_COLORS) {
-            colors.add(color)
-        }
-        for (color in ColorTemplate.VORDIPLOM_COLORS) {
-            colors.add(color)
-        }
-        val dataSet = PieDataSet(entries, "Expense Category")
-        dataSet.colors = colors
-        val data = PieData(dataSet)
-        data.setDrawValues(true)
-        data.setValueFormatter(PercentFormatter(DecimalFormat()))
-        data.setValueTextSize(12f)
-        data.setValueTextColor(Color.BLACK)
-        pieChart.data = data
-        pieChart.invalidate()
-        pieChart.animateY(1400, Easing.getEasingFunctionFromOption(Easing.EasingOption.EaseInOutQuad))
-    }
-
-    private fun setupPieChart() {
-        pieChart.isDrawHoleEnabled = true
-        pieChart.setUsePercentValues(true)
-        pieChart.setEntryLabelTextSize(12f)
-        pieChart.setEntryLabelColor(Color.BLACK)
-        pieChart.centerText = "Spending by Category"
-        pieChart.setCenterTextSize(24f)
-        pieChart.description.isEnabled = false
-        val l: Legend = pieChart.legend
-        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP)
-        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT)
-        l.setOrientation(Legend.LegendOrientation.VERTICAL)
-        l.setDrawInside(false)
-        l.setEnabled(true)
-    }
 }
