8bee383d4bdb0aa6426f9132404bd318de379d97
iyke <kuradavis987@gmail.com>
2025-01-11 22:55:30 +0100

    added app module for dependcy injection

--- app/src/main/java/com/iyke/onlinebanking/data/local/database/AppDatabase.kt
+++ app/src/main/java/com/iyke/onlinebanking/data/local/database/AppDatabase.kt
@@ -3,11 +3,20 @@ package com.iyke.onlinebanking.data.local.database
 import androidx.room.Database
 import androidx.room.Entity
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.iyke.onlinebanking.data.local.dao.BankStatementDao
 import com.iyke.onlinebanking.data.local.dao.UsersDao
 import com.iyke.onlinebanking.data.local.entries.BankStatementsEntity
 import com.iyke.onlinebanking.data.local.entries.UsersEntity
 
+
+val MIGRATION_1_2 = object : Migration(1, 2) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        // migration for adding a new column
+        database.execSQL("ALTER TABLE articles ADD COLUMN new_column_name TEXT DEFAULT ''")
+    }
+}
 
 @Database(entities = [UsersEntity::class, BankStatementsEntity::class], version = 1)
 abstract class AppDatabase: RoomDatabase() {
--- app/src/main/java/com/iyke/onlinebanking/di/AppModules.kt
+++ app/src/main/java/com/iyke/onlinebanking/di/AppModules.kt
@@ -1,4 +1,40 @@
 package com.iyke.onlinebanking.di
 
-class AppModules {
+import android.content.Context
+import androidx.room.Room
+import com.iyke.onlinebanking.data.local.dao.UsersDao
+import com.iyke.onlinebanking.data.local.database.AppDatabase
+import com.iyke.onlinebanking.data.local.database.MIGRATION_1_2
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModules {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.databaseBuilder(context, AppDatabase::class.java, AppDatabase.DATABASE_NAME)
+            .addMigrations(MIGRATION_1_2)
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providerUserDao(appDatabase: AppDatabase) = appDatabase.getUsersDao()
+
+    @Provides
+    @Singleton
+    fun providersStatementDao(appDatabase: AppDatabase) {
+        appDatabase.getBankStatementDao()
+    }
+
+
 }
